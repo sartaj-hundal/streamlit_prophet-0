b91e5996feaae6ac6e4fc7cbc5043e6b72fada09
Sartaj Hundal <sartaj.hundal@kaito.fi>
2023-02-15 10:06:17 +0200

    test

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ cs = conn.cursor()
 
 try:
     #cs.execute("SELECT * FROM MODEL_DATA LIMIT 10;")
-    cs.execute("SELECT DATABASE();")
+    cs.execute("SELECT CURRENT_DATABASE();")
     #df = pd.DataFrame(cs.fetchall(), columns = ['DATE', 'SALES'])
     #max_sales = df['SALES'].max()
     #st.write(max_sales)
